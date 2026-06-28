2e0429c2aff25cf9a150b2e59c7f4089a57656d8
ShajeebHaider <alihaidershajeeb06@gmail.com>
2026-06-28 11:38:36 +0600

    updated deploy config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export", // enables static export
   basePath: "/haroun-kuti-fc",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
