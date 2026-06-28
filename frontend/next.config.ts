6ce0c42eaad6d4de61322e755413ab2aaf2d49e7
ShajeebHaider <alihaidershajeeb06@gmail.com>
2026-06-28 11:22:52 +0600

    config files

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // enables static export
+  basePath: "/haroun-kuti-fc",
   images: {
     remotePatterns: [
       {
